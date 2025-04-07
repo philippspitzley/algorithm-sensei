157b4d9c6d414b7f76ec75587de594797ec2802c
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-04-07 09:10:51 +0200

    🔥 remove from None of HTTPException

--- app/api/deps.py
+++ app/api/deps.py
@@ -37,7 +37,7 @@ def get_current_user(session: SessionDep, token: TokenDep) -> User:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Could not validate credentials",
-        ) from None
+        )
     user = session.get(User, token_data.sub)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")

fbf3389acf18f26dcf83bed761e99040ff1a3cb7
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-04-10 12:53:02 +0200

    🗄️ Add initial database migration with new models

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -23,8 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from app.models import SQLModel # noqa
-
+from app.models import SQLModel
 target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,

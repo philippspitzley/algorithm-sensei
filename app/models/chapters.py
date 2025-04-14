287f214aae919aeadddc1cc907c38fb077fd7a93
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-04-14 13:27:24 +0200

    🦺 add HttpurlType decorator and reorganize Base classes and API classes    with new chapter_num and chapter_point_num
    - pydantic type of HttpUrl is not compatible with postgres db.
      The custom HttpurlType Decorator fixes this by writing a normal string
      to the data base. While outside of the database pydandic validation
      with HttpUrl is performed.
    
    - Base model have no optional chapter/chapter_point number anymore.
      They are required now. Removed Base inheretence from Update classes
      to make chapter/chapter_point nums optional again.

--- app/models/chapters.py
+++ app/models/chapters.py
@@ -1,9 +1,9 @@
 import uuid
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from pydantic import HttpUrl
-from sqlalchemy import Column
-from sqlmodel import Field, Relationship, SQLModel, String
+from sqlalchemy.types import String, TypeDecorator
+from sqlmodel import Field, Relationship, SQLModel
 
 from .base import TimeStampMixin
 
@@ -11,18 +11,42 @@ if TYPE_CHECKING:
     from .courses import Course
 
 
-# Shared properties
+# custom HttpUrl type
+class HttpUrlType(TypeDecorator[HttpUrl]):
+    impl = String(2083)
+    cache_ok = True
+
+    @property
+    def python_type(self):
+        return HttpUrl
+
+    def process_bind_param(self, value: HttpUrl | None, dialect: Any) -> str | None:
+        if value is None:
+            return None
+        return str(value)
+
+    def process_result_value(self, value: str | None, dialect: Any) -> HttpUrl | None:
+        if value is None:
+            return None
+        return HttpUrl(url=value)
+
+    def process_literal_param(self, value: HttpUrl | None, dialect: Any) -> str:
+        return str(value)
+
+
+# Shared required properties
 class ChapterBase(SQLModel):
+    chapter_num: int = Field(ge=1)
     title: str = Field(min_length=1, max_length=255)
     description: str | None = Field(default=None, max_length=255)
 
 
 class ChapterPointBase(SQLModel):
-    chapter_num: int | None = Field(default=1, ge=1)
+    chapter_point_num: int = Field(ge=1)
     text: str | None = Field(default=None)
     code_block: str | None = Field(default=None)
-    image: HttpUrl | None = Field(default=None, sa_column=Column(String(255)))
-    video: HttpUrl | None = Field(default=None, sa_column=Column(String(255)))
+    image: HttpUrl | None = Field(default=None, sa_type=HttpUrlType)
+    video: HttpUrl | None = Field(default=None, sa_type=HttpUrlType)
 
 
 # Database model
@@ -45,20 +69,25 @@ class ChapterPoint(TimeStampMixin, ChapterPointBase, table=True):
 
 # API Input models
 class ChapterCreate(ChapterBase):
-    course_id: uuid.UUID
-
-
-class ChapterUpdate(SQLModel):
-    title: str | None = None
-    description: str | None = None
+    pass
 
 
 class ChapterPointCreate(ChapterPointBase):
     pass
 
 
-class ChapterPointUpdate(ChapterPointBase):
-    pass
+class ChapterUpdate(SQLModel):
+    chapter_num: int | None = Field(default=None, ge=1)
+    title: str | None = Field(default=None, min_length=1, max_length=255)
+    description: str | None = Field(default=None, max_length=255)
+
+
+class ChapterPointUpdate(SQLModel):
+    chapter_point_num: int | None = Field(default=None, ge=1)
+    text: str | None = Field(default=None)
+    code_block: str | None = Field(default=None)
+    image: HttpUrl | None = Field(default=None, sa_type=HttpUrlType)
+    video: HttpUrl | None = Field(default=None, sa_type=HttpUrlType)
 
 
 class ChapterPointPublic(ChapterPointBase):
@@ -66,7 +95,17 @@ class ChapterPointPublic(ChapterPointBase):
     chapter_id: uuid.UUID
 
 
+class ChapterPointsPublic(SQLModel):
+    data: list[ChapterPointPublic] = Field(default_factory=list)
+    count: int | None = None
+
+
 class ChapterPublic(ChapterBase):
     id: uuid.UUID
     course_id: uuid.UUID
-    points: list[ChapterPointPublic] | None = Field(default=[])
+    points: list[ChapterPointPublic] = Field(default_factory=list)
+
+
+class ChaptersPublic(SQLModel):
+    data: list[ChapterPublic] = Field(default_factory=list)
+    count: int | None = None

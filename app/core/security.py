5328021eaec07ade1ab4b532fca22d321ce0a0c7
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-04-14 13:39:12 +0200

    🚨 add type missing type to create_access-token

--- app/core/security.py
+++ app/core/security.py
@@ -14,7 +14,7 @@ ALGORITHM = "HS256"
 
 def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
     expire = datetime.now(UTC) + expires_delta
-    to_encode = {"exp": expire, "sub": str(subject)}
+    to_encode: dict[str, datetime | str] = {"exp": expire, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
